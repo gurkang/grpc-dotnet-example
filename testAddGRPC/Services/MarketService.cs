19f794242ddc9ddc6415f765f7b93512a23be5f9
gurkang <gustav.segerstedt@gmail.com>
2022-04-13 15:04:37 +0300

    initial commit

---
+++ testAddGRPC/Services/MarketService.cs
@@ -0,0 +1,17 @@
+﻿namespace testAddGRPC.Services
+{
+    public class MarketService
+    {
+        public Task<market.Market> Market(market.Market market)
+        {
+            var languages = new List<market.Language>();
+            languages.Add(new market.Language());
+            return Task.FromResult(new market.Market
+            {
+                Code = "123",
+                Languages = languages,
+                Name = "123"
+            });
+        }
+    }
+}
